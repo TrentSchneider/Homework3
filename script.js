5053a374ca37281181dee52aa8eb5209d5edd794
Trenton Schneider <trentonschneider@TrentonidersMBP.fios-router.home>
2020-06-08 16:58:40 -0400

    added prompt/loop for if no character options are chosen and fixed car issue created in last commit.

--- script.js
+++ script.js
@@ -13,73 +13,89 @@ function writePassword() {
 			number = "0123456789",
 			special = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 			passLength,
-			passChar = "",
+			passChar,
 			useLowCase,
 			useUpCase,
 			useNum,
 			useSpec,
 			userPass = "";
-
 		do {
-			var useLowCase = prompt(
-				"Would you like lower case letters in your password? (Please type Yes or No)"
-			);
+			do {
+				var useLowCase = prompt(
+					"Would you like lower case letters in your password? (Please type Yes or No)"
+				);
 
-			var useLowCase = useLowCase.toLowerCase();
+				var useLowCase = useLowCase.toLowerCase();
 
-			if (useLowCase === "yes") {
-				passChar += lower;
-				alert("Lower case letters will be used.");
-			} else if (useLowCase === "no") {
-				alert("Lower case letters will not be used.");
+				if (useLowCase === "yes") {
+					passChar += lower;
+					alert("Lower case letters will be used.");
+				} else if (useLowCase === "no") {
+					alert("Lower case letters will not be used.");
+				}
+			} while (useLowCase !== "yes" && useLowCase !== "no");
+
+			do {
+				var useUpCase = prompt(
+					"Would you like upper case letters in your password? (Please type Yes or No)"
+				);
+
+				var useUpCase = useUpCase.toLowerCase();
+
+				if (useUpCase === "yes") {
+					passChar += upper;
+					alert("Upper case letters will be used.");
+				} else if (useUpCase === "no") {
+					alert("Upper case letters will not be used.");
+				}
+			} while (useUpCase !== "yes" && useUpCase !== "no");
+
+			do {
+				var useNum = prompt(
+					"Would you like numbers in your password? (Please type Yes or No)"
+				);
+
+				var useNum = useNum.toLowerCase();
+
+				if (useNum === "yes") {
+					passChar += number;
+					alert("Numbers will be used.");
+				} else if (useNum === "no") {
+					alert("Numbers will not be used.");
+				}
+			} while (useNum !== "yes" && useNum !== "no");
+
+			do {
+				var useSpec = prompt(
+					"Would you like special characters (!@#) in your password? (Please type Yes or No)"
+				);
+
+				var useSpec = useSpec.toLowerCase();
+
+				if (useSpec === "yes") {
+					passChar += special;
+					alert("Special characters will be used.");
+				} else if (useSpec === "no") {
+					alert("Special characters will not be used.");
+				}
+			} while (useSpec !== "yes" && useSpec !== "no");
+
+			if (
+				useLowCase === "no" &&
+				useUpCase === "no" &&
+				useNum === "no" &&
+				useSpec === "no"
+			) {
+				alert(
+					"No character options have been selected, please choose at least one character option."
+				);
 			}
-		} while (useLowCase !== "yes" && useLowCase !== "no");
-
-		do {
-			var useUpCase = prompt(
-				"Would you like upper case letters in your password? (Please type Yes or No)"
-			);
-
-			var useUpCase = useUpCase.toLowerCase();
-
-			if (useUpCase === "yes") {
-				passChar += upper;
-				alert("Upper case letters will be used.");
-			} else if (useUpCase === "no") {
-				alert("Upper case letters will not be used.");
-			}
-		} while (useUpCase !== "yes" && useUpCase !== "no");
-
-		do {
-			var useNum = prompt(
-				"Would you like numbers in your password? (Please type Yes or No)"
-			);
-
-			var useNum = useNum.toLowerCase();
-
-			if (useNum === "yes") {
-				passChar += number;
-				alert("Numbers will be used.");
-			} else if (useNum === "no") {
-				alert("Numbers will not be used.");
-			}
-		} while (useNum !== "yes" && useNum !== "no");
-
-		do {
-			var useSpec = prompt(
-				"Would you like special characters (!@#) in your password? (Please type Yes or No)"
-			);
-
-			var useSpec = useSpec.toLowerCase();
-
-			if (useSpec === "yes") {
-				passChar += special;
-				alert("Special characters will be used.");
-			} else if (useSpec === "no") {
-				alert("Special characters will not be used.");
-			}
-		} while (useSpec !== "yes" && useSpec !== "no");
-
+		} while (
+			useLowCase === "no" &&
+			useUpCase === "no" &&
+			useNum === "no" &&
+			useSpec === "no"
+		);
 		do {
 			var passLength = parseInt(
 				prompt(
