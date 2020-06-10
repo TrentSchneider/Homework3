4aad5743fb6434a9622136545ecca3fe1cda25cd
Trenton Schneider <trentonschneider@TrentonidersMBP.fios-router.home>
2020-06-10 12:26:49 -0400

    began process of converting from prompt to toggle switches and sliders. commented out prompt specific code and added toggle switch specific variables.

--- script.js
+++ script.js
@@ -14,99 +14,104 @@ function writePassword() {
 			special = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 			passLength,
 			passChar,
-			useLowCase,
-			useUpCase,
-			useNum,
-			useSpec,
+			// useLowCase,
+			// useUpCase,
+			// useNum,
+			// useSpec,
+			lowTog = document.getElementById("lowChoice"),
+			upTog = document.getElementById("upChoice"),
+			numTog = document.getElementById("numChoice"),
+			specTog = document.getElementById("specChoice"),
 			userPass = "";
-		do {
-			do {
-				var useLowCase = prompt(
-					"Would you like lower case letters in your password? (Please type Yes or No)"
-				);
 
-				var useLowCase = useLowCase.toLowerCase();
+		// do {
+		// 	do {
+		// 		var useLowCase = prompt(
+		// 			"Would you like lower case letters in your password? (Please type Yes or No)"
+		// 		);
 
-				if (useLowCase === "yes") {
-					passChar += lower;
-					alert("Lower case letters will be used.");
-				} else if (useLowCase === "no") {
-					alert("Lower case letters will not be used.");
-				}
-			} while (useLowCase !== "yes" && useLowCase !== "no");
+		// 		var useLowCase = useLowCase.toLowerCase();
 
-			do {
-				var useUpCase = prompt(
-					"Would you like upper case letters in your password? (Please type Yes or No)"
-				);
+		// 		if (useLowCase === "yes") {
+		// 			passChar += lower;
+		// 			alert("Lower case letters will be used.");
+		// 		} else if (useLowCase === "no") {
+		// 			alert("Lower case letters will not be used.");
+		// 		}
+		// 	} while (useLowCase !== "yes" && useLowCase !== "no");
 
-				var useUpCase = useUpCase.toLowerCase();
+		// 	do {
+		// 		var useUpCase = prompt(
+		// 			"Would you like upper case letters in your password? (Please type Yes or No)"
+		// 		);
 
-				if (useUpCase === "yes") {
-					passChar += upper;
-					alert("Upper case letters will be used.");
-				} else if (useUpCase === "no") {
-					alert("Upper case letters will not be used.");
-				}
-			} while (useUpCase !== "yes" && useUpCase !== "no");
+		// 		var useUpCase = useUpCase.toLowerCase();
 
-			do {
-				var useNum = prompt(
-					"Would you like numbers in your password? (Please type Yes or No)"
-				);
+		// 		if (useUpCase === "yes") {
+		// 			passChar += upper;
+		// 			alert("Upper case letters will be used.");
+		// 		} else if (useUpCase === "no") {
+		// 			alert("Upper case letters will not be used.");
+		// 		}
+		// 	} while (useUpCase !== "yes" && useUpCase !== "no");
 
-				var useNum = useNum.toLowerCase();
+		// 	do {
+		// 		var useNum = prompt(
+		// 			"Would you like numbers in your password? (Please type Yes or No)"
+		// 		);
 
-				if (useNum === "yes") {
-					passChar += number;
-					alert("Numbers will be used.");
-				} else if (useNum === "no") {
-					alert("Numbers will not be used.");
-				}
-			} while (useNum !== "yes" && useNum !== "no");
+		// 		var useNum = useNum.toLowerCase();
 
-			do {
-				var useSpec = prompt(
-					"Would you like special characters (!@#) in your password? (Please type Yes or No)"
-				);
+		// 		if (useNum === "yes") {
+		// 			passChar += number;
+		// 			alert("Numbers will be used.");
+		// 		} else if (useNum === "no") {
+		// 			alert("Numbers will not be used.");
+		// 		}
+		// 	} while (useNum !== "yes" && useNum !== "no");
 
-				var useSpec = useSpec.toLowerCase();
+		// 	do {
+		// 		var useSpec = prompt(
+		// 			"Would you like special characters (!@#) in your password? (Please type Yes or No)"
+		// 		);
 
-				if (useSpec === "yes") {
-					passChar += special;
-					alert("Special characters will be used.");
-				} else if (useSpec === "no") {
-					alert("Special characters will not be used.");
-				}
-			} while (useSpec !== "yes" && useSpec !== "no");
+		// 		var useSpec = useSpec.toLowerCase();
 
-			if (
-				useLowCase === "no" &&
-				useUpCase === "no" &&
-				useNum === "no" &&
-				useSpec === "no"
-			) {
-				alert(
-					"No character options have been selected, please choose at least one character option."
-				);
-			}
-		} while (
-			useLowCase === "no" &&
-			useUpCase === "no" &&
-			useNum === "no" &&
-			useSpec === "no"
-		);
-		do {
-			var passLength = parseInt(
-				prompt(
-					"How long would you like your password to be? (Please choose a number between 8 and 128, decimals will be rounded down to the nearest whole number)"
-				),
-				10
-			);
-			if (Number.isInteger(passLength) && passLength > 7 && passLength < 129) {
-				alert("Your password will be " + passLength + " characters long.");
-			}
-		} while (isNaN(passLength) || passLength < 8 || passLength > 128);
+		// 		if (useSpec === "yes") {
+		// 			passChar += special;
+		// 			alert("Special characters will be used.");
+		// 		} else if (useSpec === "no") {
+		// 			alert("Special characters will not be used.");
+		// 		}
+		// 	} while (useSpec !== "yes" && useSpec !== "no");
+
+		// 	if (
+		// 		useLowCase === "no" &&
+		// 		useUpCase === "no" &&
+		// 		useNum === "no" &&
+		// 		useSpec === "no"
+		// 	) {
+		// 		alert(
+		// 			"No character options have been selected, please choose at least one character option."
+		// 		);
+		// 	}
+		// } while (
+		// 	useLowCase === "no" &&
+		// 	useUpCase === "no" &&
+		// 	useNum === "no" &&
+		// 	useSpec === "no"
+		// );
+		// do {
+		// 	var passLength = parseInt(
+		// 		prompt(
+		// 			"How long would you like your password to be? (Please choose a number between 8 and 128, decimals will be rounded down to the nearest whole number)"
+		// 		),
+		// 		10
+		// 	);
+		// 	if (Number.isInteger(passLength) && passLength > 7 && passLength < 129) {
+		// 		alert("Your password will be " + passLength + " characters long.");
+		// 	}
+		// } while (isNaN(passLength) || passLength < 8 || passLength > 128);
 
 		for (let i = 0; i < passLength; i++) {
 			userPass += passChar.charAt(Math.floor(Math.random() * passChar.length));
